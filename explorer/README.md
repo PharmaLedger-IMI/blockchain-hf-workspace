dda659988cf5c76c5870d8f67e55cd76e7a0c7b8
Francisco Damiao <Francisco.Damiao@pdmfc.com>
2022-05-03 11:42:32 +0100

    Added Hyperledger Explorer support.

---
+++ explorer/README.md
@@ -0,0 +1,6 @@
+# Hyperledger Explorer
+
+To use it, just run the **start.sh** script. All other files are pre-configured for the test network.
+
+The web interface available at port 8080, requires no user authentication.
+
---
+++ explorer/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Configuring network..."
+#cp -r ../fabric-samples/test-network/organizations/ .
+ADMINPK=`ls ../fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin\@org1.example.com/msp/keystore/*_sk | cut -d '/' -f 11`
+echo ${ADMINPK}
+
+cat connection-profile/template.json | sed "s,PRIVSK,${ADMINPK},g" > connection-profile/test-network.json
+
+# Na ultima versão do docker o compose já faz parte dos comandos do docker
+docker -d compose up
+
+# Versão antiga
+#docker-compose up
+
